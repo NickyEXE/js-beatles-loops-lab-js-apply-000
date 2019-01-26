93c6df1ba6d6537ea8f8840609484cafc1259bcb
Nicholas Dover <dovern42@gmail.com>
2019-01-26 19:12:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var whatTheyPlay = []
+  var i = 0
+  for (i=0; i < musicians.length; i++) {
+    whatTheyPlay.push(musicians[i] + " plays " + instruments[i])}
+    return whatTheyPlay}
+
+
+//theBeatlesPlay
+//var beatles = ["John", "Paul", "George", "Ringo", "Yoko"]
+//var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "poorly", "My Heart"]
+//console.log (theBeatlesPlay(beatles,instruments))
+//console.log ("the length is correct: " + (theBeatlesPlay(beatles,instruments).length === 5))
+
